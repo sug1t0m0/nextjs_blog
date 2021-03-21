0bc6a841b02e0de7464dcc45c03a389902fbed02
sugiyama.t <sugiyama.t@water-cell.jp>
2021-03-21 20:39:36 +0900

    静的生成を実装

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/global.css'
 
-export default function App ({ Component, PageProps }) {
-    return <Component {...PageProps} />
+export default function App ({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
